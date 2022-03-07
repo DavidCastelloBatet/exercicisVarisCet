763597489469bfb98bd5cd86997e4db729ee2970
David Castello <davidcastellobatetgmail.com>
2022-03-07 12:41:48 +0100

    agafa nomes fills de head i body

--- contar elementos del dom/index.js
+++ contar elementos del dom/index.js
@@ -1,31 +1,22 @@
-// con document.all funcionaria pero està en desuso
-// let arrayDom = Object.values(document.all);
+function myFunction() {
 
-// Al metodo values del objeto Object le pasamos como parametros
-// todos los tags del documento con document.querySelectorAll('*')
-let arrayDom = Object.values(document.querySelectorAll('*'));
-console.log(arrayDom);
-
-// Transformarlos en un array
-let nameTag = new Array();
-for (let i = 0; i < arrayDom.length; i++) {
-  nameTag.push(arrayDom[i].localName);
-}
-console.log(nameTag)
-
-// juntar y contar los elementos
-let contador = nameTag.reduce((acumulador, valorActualnameTag) => 
-(acumulador[valorActualnameTag] ? 
-acumulador[valorActualnameTag] + 1 :
-acumulador[valorActualnameTag] = 1, acumulador ), {});
-console.log(contador);
-
-// Mostrar los elementos
-var text = "";
-for(var [etiqueta, cantidad] of Object.entries(contador))
-{
-    console.log(etiqueta,":",cantidad);
   
+  var elements = document.head.children;
+  var txt = "";
+  var i;
+  for (i = 0; i < elements.length; i++) {
+    txt = txt + elements[i].tagName + "<br>";
+  }
+
+  document.getElementById("head").innerHTML = txt.toLowerCase();
+
+
+  var elements = document.body.children;
+  var txt = "";
+  var i;
+  for (i = 0; i < elements.length; i++) {
+    txt = txt + elements[i].tagName + "<br>";
+  }
+
+  document.getElementById("body").innerHTML = txt.toLowerCase();
 }
-
-
